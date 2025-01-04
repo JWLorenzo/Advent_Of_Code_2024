5695dda5ad415f6265c81dc44aaae5da0019b33a
JacobWLorenzo <jacob.w.lorenzo@maine.edu>
2025-01-04 01:46:28 -0500

    Day_3 Done
    Changes to be committed:
    new file:   Day_3/Part_1/main.py
    new file:   Day_3/Part_1/test.txt
    new file:   Day_3/Part_1/test2.txt
    new file:   Day_3/Part_2/main.py
    new file:   Day_3/Part_2/test.txt
    new file:   Day_3/Part_2/test2.txt

---
+++ Day_3/Part_2/main.py
@@ -0,0 +1,41 @@
+with open("test.txt", "r") as f:
+    lines = f.readlines()
+
+
+def main() -> None:
+    sum_storage = 0
+    do = True
+    for line in lines:
+        # print(line)
+        for char in range(len(line)):
+            if line[char : char + 7] == "don't()" and do:
+                do = False
+            if line[char : char + 4] == "do()" and not do:
+                do = True
+
+            if line[char : char + 4] == "mul(" and do:
+                store_int = ["", ""]
+                index_to_store = 0
+                for i in range(char + 4, len(line)):
+                    if line[i] == "," and index_to_store == 0:
+                        index_to_store += 1
+                    elif line[i] == ")" and index_to_store == 1:
+                        print(store_int)
+                        if store_int[0].isdigit() and store_int[1].isdigit():
+                            sum_storage += int(store_int[0]) * int(store_int[1])
+                            print(store_int)
+                            break
+                        else:
+                            break
+                    elif line[i].isdigit():
+                        if store_int[index_to_store] == "" and line[i] == "0":
+                            pass
+                        else:
+                            store_int[index_to_store] += line[i]
+                    else:
+                        break
+    print(sum_storage)
+
+
+if __name__ == "__main__":
+    main()
